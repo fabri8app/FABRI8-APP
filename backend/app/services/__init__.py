da0626e18aa9dea5958934c4bdb55b88c71f51f5
fabri8app <fabri8.app@gmail.com>
2025-12-18 15:29:58 +0530

    Delete backend directory

--- backend/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services Package
-"""
-
-from .ai_service import AIService
-from .websocket_service import WebSocketService
-from .file_manager import FileManager
-
-__all__ = ['AIService', 'WebSocketService', 'FileManager']
